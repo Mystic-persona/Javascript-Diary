dab62d1e9150cb744e3d2bf4a4248348b336d05c
Mystic-persona <sushreesangeeta2005@gmail.com>
2024-09-01 11:28:42 +0530

    Added file

--- Basics/datatype.js
+++ Basics/datatype.js
@@ -1,6 +1,6 @@
 "use strict";  //treat all JS code as newer version
 
-alert("Hey")  //we are using node.js not browser
+//alert("Hey")  //we are using node.js not browser
 
 console.log(3+3)
 console.log("HEY,GOOD MORNING")
@@ -16,4 +16,42 @@ let temperature = null         //null =>standalone value
 //object
 console.log(typeof undefined);   //undefined
 console.log(typeof null);     //object
+
+
+//*************DataTypes Summary***************//
+
+//Primitive
+
+// 7 types  : String, Number , Boolean, null, undefined, Symbol,Bigint
+
+const score = 100
+const scorevalue = 100.3
+const isLogIn = false
+const outsideTemp = null
+
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);   //false
+
+const bignumber = 986436925776n
+
+//Reference (Non Primitive)
+//Array , Object , Functions
+
+const subjects = ["Physics","Chemistry","Maths"];
+let myobj = {
+    name:"Sushree",
+    age: 19
+}
+
+const myfun = function(){
+    console.log(" Hey Sushree");
+}
+
+console.log(typeof subjects);   //object
+console.log(typeof myobj);      //object
+console.log(typeof myfun);      //function
+console.log(typeof id);        //symbol
+console.log(typeof anotherId); //symbol
 
---
+++ Basics/operation.js
@@ -0,0 +1,30 @@
+let value = 1
+let negvalue = -value
+console.log(negvalue);
+
+console.log(3+3);
+console.log(3-3);
+console.log(3*3);
+console.log(3/3);
+console.log(3%3);
+console.log(3**3);
+
+let str1 = "Sushree"
+let str2 = "Sangeeta"
+
+let str3 = str1+str2
+console.log(str3)
+
+console.log("1" + 2)    //12
+console.log(1 + "2")    //12
+console.log("1" + 2 + 2)   //122
+console.log(1 + 2 + "2")   //32
+
+// console.log(+true)   //1
+// console.log(+"")     //0
+
+let count = 10
+++count;
+console.log(count);   //11
+
+
