4c77759b0d77f009c0da00d25b207b1241982592
Mystic-persona <sushreesangeeta2005@gmail.com>
2024-09-01 09:34:02 +0530

    Added files

---
+++ Basics/datatype.js
@@ -0,0 +1,19 @@
+"use strict";  //treat all JS code as newer version
+
+alert("Hey")  //we are using node.js not browser
+
+console.log(3+3)
+console.log("HEY,GOOD MORNING")
+
+let name = "Mansi"             //string =>"" 
+let age = 19                   //number 2 to power 53  //bigint
+let isLoggedIn = false         //boolean =>true/false  
+let state;                     //undefined   
+let temperature = null         //null =>standalone value 
+
+//symbol => unique
+
+//object
+console.log(typeof undefined);   //undefined
+console.log(typeof null);     //object
+
